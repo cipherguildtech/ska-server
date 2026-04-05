fb40fb672c81174310398e93f27a596936e3b80a
karthikeyan <karthikeyan2642005@gmail.com>
2026-04-05 21:06:41 +0530

    customers module

--- src/customers/customers.service.ts
+++ src/customers/customers.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class CustomersService {
-
-    getCustomers() {
-
+    constructor(private prisma: PrismaService) {}
+    async getCustomers() {
+        await this.prisma.customer.findMany
     }
 
     createCustomer() {
